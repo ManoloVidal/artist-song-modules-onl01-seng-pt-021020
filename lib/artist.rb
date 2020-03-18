7c1b0ca7715949e828624dfe54df45f342f0e3db
Juan Vidal-Ramos <vidalramosjm@gmail.com>
2020-03-18 02:43:50 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,10 @@
 require 'pry'
 
+extend Memorable
+
 class Artist
+  
+  
   attr_accessor :name
   attr_reader :songs
 
@@ -19,14 +23,7 @@ class Artist
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
-
+  
   def add_song(song)
     @songs << song
     song.artist = self
